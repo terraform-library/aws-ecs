75cf231319a17668ef7bb15805712d03bad00fbb
amjad <amjad.hussain@tajawal.com>
2018-06-10 12:17:05 +0400

    update

--- variables.tf
+++ variables.tf
@@ -1,3 +1,24 @@
+variable "name" {
+  description = "Name  (e.g. `bastion` or `db`)"
+  type        = "string"
+}
+
+variable "environment" {
+  description = "Environment (e.g. `prod`, `dev`, `staging`)"
+  type        = "string"
+}
+
+variable "managedby" {
+  description = "Managedby (e.g. `cp` or `terraform-library`)"
+  type        = "string"
+}
+
+variable "delimiter" {
+  type        = "string"
+  default     = "-"
+  description = "Delimiter to be used between `name`, `environment`, `managedby`, etc."
+}
+
 variable "ses_domain" {
   description = "Domain to use for SES"
 }
